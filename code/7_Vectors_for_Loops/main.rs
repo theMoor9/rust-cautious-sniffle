f1ee6e05df5fece47ab869900d8213d48df67812
Kenneth <kennethboldrini9@gmail.com>
2024-07-07 19:09:21 +0200

    Data collections update
    Version: 0.6.0

--- code/7_Vectors_for_Loops/main.rs
+++ code/7_Vectors_for_Loops/main.rs
@@ -1,15 +1,16 @@
 /*
-Topic: Vectors
+ Topic: Vectors
 
-Requirements:
-* Print 5, 15, "twenty-five", and 35 in a loop
-* Print the total number of items in a vector
+ Requirements:
+ * Add 4 numbers (7, 14, 21, 28) to a vector
+ * Print 7, 14, "twenty-one", and 28 in a loop
+ * Print the total number of items in the vector
 
-Notes:
-* Use a vector to store 4 items
-* Iterate through the vector using a for..in loop
-* Decide whether to print the number or print "twenty-five" inside the loop
-* Use the .len() function to print the number of items
+ Notes:
+ * Use a vector to store 4 items
+ * Iterate through the vector using a for..in loop
+ * Decide whether to print the number or print "twenty-one" inside the loop
+ * Use the .len() function to print the number of items
 */
 
 
@@ -17,12 +18,29 @@ Notes:
 //YOUR VERSION----------------------------------------------------------------------------------------------------------------
 
 fn your_main () {
+
 }
 
 // MY VERSION------------------------------------------------------------------------------------------------------------------
 
-fn my_main () S{
+fn my_main () {
+
+    let mut sundays: Vec<u8> = Vec::new();
+    let mut day: u8 = 7;
+
+    while day <= 28 {
+        sundays.push(day);
+        day = day + 7;
+    }
+
+    for days in &sundays {
+        match days {
+            21 => println!("Sunday Twenty-one"),
+            _ => println!("Sunday {}", days), 
+        }
+    }
     
+    println!("Sundays in this month: {}", sundays.len());
 }
 
 
--- code/8_Strings/main.rs
+++ code/8_Strings/main.rs
@@ -1,16 +1,16 @@
 /*
 Topic: Strings
 
-Requirements:
-* Print out the name and preferred colors of individuals aged 12 and below
+ Requirements:
+ * Print out the name and preferred colors of individuals aged 12 and below
 
-Notes:
-* Use a struct for a person's age, name, and preferred color
-* The color and name should be stored as a String
-* Create and store at least 3 individuals in a vector
-* Iterate through the vector using a for..in loop
-* Use an if expression to determine which individual's info should be printed
-* The name and colors should be printed using a function
+ Notes:
+ * Use a struct for a person's age, name, and preferred color
+ * The color and name should be stored as a String
+ * Create and store at least 3 individuals in a vector
+ * Iterate through the vector using a for..in loop
+ * Use an if expression to determine which individual's info should be printed
+ * The name and colors should be printed using a function
 */
 
 
@@ -23,9 +23,43 @@ fn your_main () {
 
 // MY VERSION------------------------------------------------------------------------------------------------------------------
 
-fn my_main () {
+struct Human {
+    name: String,
+    age: u8,
+    color: String,
+}
 
-    
+impl Human {
+
+    fn new (name: String, age: u8, color: String) -> Self {
+        Human{
+            name,
+            age,
+            color,
+        }
+    }
+
+    fn tell_color (&self) {
+        println!("{}'s favourite color is {}!",self.name ,self.color );
+    }
+}
+
+fn my_main () {
+    let mut individuals: Vec<Human> = Vec::with_capacity(3);
+
+    let person1 = Human::new(String::from("Kenneth"), 27, String::from("Purple"));
+    let person2 = Human::new(String::from("Philip"), 22, String::from("Green"));
+    let person3 = Human::new(String::from("Linda"), 25, String::from("Black"));
+
+    individuals.push(person1);
+    individuals.push(person2);
+    individuals.push(person3);
+
+    for soul in individuals {
+        if soul.name == "Linda"{
+            Human::tell_color(&soul);
+        }
+    }
 }
 
 //MAIN-----------------------------------------------------------------------------------------------------------------------
