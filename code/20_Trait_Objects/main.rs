bb80b44861b9fd5e33721ad5a646075a08c79b4f
Kenneth <kennethboldrini9@gmail.com>
2024-08-26 12:50:58 +0200

    Trait Objects Exercise | Patch update
    Version: 0.9.8

--- code/20_Trait_Objects/main.rs
+++ code/20_Trait_Objects/main.rs
@@ -22,8 +22,95 @@ Notes:
 fn your_main () {}
 
 // MY VERSION------------------------------------------------------------------------------------------------------------------
+/*
+Gli elementi che necessitano di un tratto che li renda dinamici in runtime sono:
+- Carpet
+- Tile
+- Wood
+*/
+struct Carpet{
+  name: String,
+  price: u64,
+  m2: u64
+}
+struct Tile{
+  name: String,
+  price: u64,
+  m2: u64
+}
+struct Wood{
+  name: String,
+  price: u64,
+  m2: u64
+}
+trait Cost {
+  fn retrieve_cost(&self) -> u64;
+  fn retrieve_name(&self) -> String;
+  fn retrieve_qty(&self) -> u64;
+}
+impl Cost for Carpet {
+  fn retrieve_cost(&self) -> u64 {
+    self.price * self.m2
+  }
+  fn retrieve_name(&self) -> String {
+    self.name.clone()
+  }
+  fn retrieve_qty(&self) -> u64 {
+    self.m2
+  }
+}
+impl Cost for Tile {
+  fn retrieve_cost(&self) -> u64 {
+    self.price * self.m2
+  }
+  fn retrieve_name(&self) -> String {
+    self.name.clone()
+  }
+  fn retrieve_qty(&self) -> u64 {
+    self.m2
+  }
+}
+impl Cost for Wood {
+  fn retrieve_cost(&self) -> u64 {
+    self.price * self.m2
+  }
+  fn retrieve_name(&self) -> String {
+    self.name.clone()
+  }
+  fn retrieve_qty(&self) -> u64 {
+    self.m2
+  }
+}
+fn total_cost(list: Vec<Box<dyn Cost>>) -> u64 {
+  let mut cost = 0;
+  for i in list {
+    cost = cost + i.retrieve_cost();
+  }
+  cost
+}
 
-fn my_main () {
+
+fn my_main() {
+  
+  let persian_carpet = Carpet{name: String::from("Persian Carpet"),price:2000,m2:5}; // 10k
+  let marmo_di_carrara = Tile{name: String::from("Marmo di Carrara"),price:500,m2:30}; // 15k
+  let cameroon_ebony = Wood{name: String::from("Cameroon's Ebony"),price:2500,m2:5};    // 12.5k
+
+  let material_list: Vec<Box<dyn Cost>> = vec![
+    Box::new(marmo_di_carrara),
+    Box::new(cameroon_ebony),
+    Box::new(persian_carpet)
+  ];
+
+  for (index,item) in material_list.iter().enumerate() {
+    println!("Cart's item N°{}\n{} m2 of {}\nCost:{}€\n", 
+      index+1,
+      item.retrieve_qty(),
+      item.retrieve_name(),
+      item.retrieve_cost()
+    );
+  }
+  println!("Total cart's cost: {}€",  total_cost(material_list)); // 37.5k
 
 }
 
